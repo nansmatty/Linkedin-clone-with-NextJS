fd830a38994af1e7b5879ff35d6d6f0292dc0233
nansmatty <narayanmaitysp1997@gmail.com>
2022-01-22 17:33:39 +0530

    first commit

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <meta charset="UTF-8" />
+        <title>LinkedIn</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+    </div>
+  );
+}
